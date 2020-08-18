3b699327258a3f1a00e2f812ff5fcecf53dee172
YEFERSON DAVID MURCIA MIRANDA <62619296+ymurciami@users.noreply.github.com>
2020-08-18 16:36:58 -0500

    Obtener datos del usuario desde el formulario

--- formulario/formulario/urls.py
+++ formulario/formulario/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from perfiles.views import formulario
+from perfiles.views import formulario, registro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('formulario', formulario),
+    path('registro/', registro),
 ]
--- formulario/perfiles/views.py
+++ formulario/perfiles/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def formulario(request):
-    return render(request, "formulario.html",{"Nombre":"Jose"})
+    return render(request, "formulario.html")
+
+def registro(request):
+    mensaje="El nombre introducido es: %r" %request.GET["nombre"]
+    return HttpResponse(mensaje)
