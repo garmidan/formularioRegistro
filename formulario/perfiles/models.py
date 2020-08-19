e83c46f642998567ca6ee03c016731ae02dcf40d
“Garmidan” <garmidan16@gmail.com>
2020-08-19 13:10:47 -0500

    Validacion campos,cedula y registro a la BD

--- formulario/perfiles/models.py
+++ formulario/perfiles/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class City(models.Model):
-    nombre = models.CharField()
+    nombre = models.CharField(max_length=30)
 
-class Users(models.Model):
-    nombres = models.CharField()
-    apellidos = models.CharField()
+class Usuario(models.Model):
+    nombres = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=40)
     cedula = models.IntegerField()
     telefono = models.CharField(max_length=10)
     mail = models.EmailField()
-    ip = models.CharField()
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    ip = models.CharField(max_length=30)
+    ciudad = models.ForeignKey(City, on_delete=models.CASCADE)
 
--- formulario/perfiles/views.py
+++ formulario/perfiles/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from perfiles.models import Usuario,City
 # Create your views here.
+
+
+
 def formulario(request):
-    return render(request, "formulario.html")
+    lista = Usuario.objects.all
+    ciudades = City.objects.all
+    return render(request, "formulario.html",{"lista":lista,"ciudades":ciudades})
 
 def registro(request):
-    mensaje="El nombre introducido es: %r" %request.GET["nombre"]
-    return HttpResponse(mensaje)
+    validacion = 0
+    nombre = ""
+    nombre= request.GET["nombre"]
+    apellidos = request.GET["apellidos"]
+    cedula = request.GET["cedula"]
+    telefono = request.GET["telefono"]
+    email = request.GET["email"]
+    idciudad = request.GET["ciudad"]
+    if nombre and apellidos and telefono and cedula and email:
+        lista = Usuario.objects.all
+        ciudades = City.objects.all
+        cedulavalidacion = Usuario.objects.filter(cedula = cedula)
+        if cedulavalidacion:
+            validacion = 3
+            return render(request,"formulario.html",{"validacion":validacion,"ciudades":ciudades,"lista":lista})
+        else:
+            ciudad = City(id = idciudad)
+            usuario = Usuario(nombres = nombre, apellidos = apellidos, cedula = cedula, telefono = telefono, mail = email, ip = "1.0920", ciudad=ciudad)
+            usuario.save()
+            validacion = 1
+            return render(request,"formulario.html",{"validacion":validacion,"ciudades":ciudades,"lista":lista})
+    else:
+        validacion = 2
+        return render(request,"formulario.html",{"validacion":validacion,"apellidos":apellidos,"ciudades":ciudades,"lista":lista})
+  
