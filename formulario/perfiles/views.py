ebf637513ad8f7ebbfdc0e7adc479c58bebf1694
“Garmidan” <garmidan16@gmail.com>
2020-08-19 15:35:17 -0500

    Ejercicio completado

--- formulario/perfiles/views.py
+++ formulario/perfiles/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from perfiles.models import Usuario,City
+import socket
 # Create your views here.
-
-
 
 def formulario(request):
     lista = Usuario.objects.all
     ciudades = City.objects.all
     return render(request, "formulario.html",{"lista":lista,"ciudades":ciudades})
 
+#Metodo registro
 def registro(request):
+    nombre_equipo = socket.gethostname()
+    direccion_equipo = socket.gethostbyname(nombre_equipo)
     validacion = 0
     nombre = ""
     nombre= request.GET["nombre"]
@@ -22,15 +24,23 @@ def registro(request):
     if nombre and apellidos and telefono and cedula and email:
         lista = Usuario.objects.all
         ciudades = City.objects.all
-        cedulavalidacion = Usuario.objects.filter(cedula = cedula)
-        if cedulavalidacion:
-            validacion = 3
-            return render(request,"formulario.html",{"validacion":validacion,"ciudades":ciudades,"lista":lista})
+        if nombre.isalpha() and apellidos.isalpha():
+            try:
+                cedulavalidacion = Usuario.objects.filter(cedula = cedula)
+                if cedulavalidacion:
+                    validacion = 3
+                    return render(request,"formulario.html",{"validacion":validacion,"ciudades":ciudades,"lista":lista})
+                else:
+                    ciudad = City(id = idciudad)
+                    usuario = Usuario(nombres = nombre, apellidos = apellidos, cedula = cedula, telefono = telefono, mail = email, ip = direccion_equipo, ciudad=ciudad)
+                    usuario.save()
+                    validacion = 1
+                    return render(request,"formulario.html",{"validacion":validacion,"ciudades":ciudades,"lista":lista})
+            except ValueError:
+                validacion = 5
+                return render(request,"formulario.html",{"validacion":validacion,"ciudades":ciudades,"lista":lista})
         else:
-            ciudad = City(id = idciudad)
-            usuario = Usuario(nombres = nombre, apellidos = apellidos, cedula = cedula, telefono = telefono, mail = email, ip = "1.0920", ciudad=ciudad)
-            usuario.save()
-            validacion = 1
+            validacion = 4
             return render(request,"formulario.html",{"validacion":validacion,"ciudades":ciudades,"lista":lista})
     else:
         validacion = 2
